0b7329dff1962a3835045fff8b673329a157b6f1
ciarasastre <sastreciara@hotmail.com>
2019-03-26 10:11:33 +0000

    Attempted Finite state machine but it didnt work ill keep it in so you can see where i was going with it

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -12,6 +12,7 @@ public class Base : MonoBehaviour
 
     public GameObject fighterPrefab;
     public GameObject Spawn;
+    public GameObject fighter;
 
 
     // Start is called before the first frame update
@@ -21,7 +22,10 @@ public class Base : MonoBehaviour
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         }
-        
+
+        //While application is still running
+        StartCoroutine(TiberiumBase());
+
     }
 
     // Update is called once per frame
@@ -30,12 +34,14 @@ public class Base : MonoBehaviour
         //text.text = "" + tiberium; Not working yet as i am trying to figure out text first
 
         //For every second that passes add 5 units of tiberium
-        StartCoroutine(TiberiumBase());
+        //StartCoroutine(TiberiumBase());
 
         //Check if the unit == 10 then spawn a fighter ship
         if(tiberium == 10)
         {
-            Instantiate(fighterPrefab, Spawn.transform.position, Quaternion.identity);
+            fighter = Instantiate(fighterPrefab, Spawn.transform.position, Quaternion.identity);
+            tiberium = tiberium - 10;
+            
         }
     }
 
---
+++ Assets/FighterAi.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterAi : MonoBehaviour
+{
+
+    public GameObject spawn;
+    public GameObject target;
+
+    public float speed;
+
+    public GameObject[] waypoints;
+    public int waypointIndex;
+
+    public enum State
+    {
+        SEEK,
+        ATTACK,
+        REFUEL
+    }
+
+    public State state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = FighterAi.State.SEEK;
+        StartCoroutine("FSM");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IEnumerator FSM()
+        {
+            switch(state)
+            {
+                case State.SEEK:
+                    Seek();
+                    break;
+
+                case State.ATTACK:
+                    Attack();
+                    break;
+
+                case State.REFUEL:
+                    Refuel();
+                    break;
+
+            }
+
+            yield return null;
+        }
+    }
+
+    void Seek()
+    {
+        //Fighter is going to wander to find a base
+        //if(Vector3.Distance(transform.position, waypoints[waypointIndex].transform.position <= 2))
+        // {
+
+        // }
+
+        target = waypoints[0];
+        float dist = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(spawn.transform.position, target.transform.position, speed);
+
+
+
+    }
+
+    void Attack()
+    {
+
+    }
+
+    void Refuel()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        //If you collide with an enemy base start attacking
+        if(col.tag == "Enemy")
+        {
+            state = FighterAi.State.ATTACK;
+        }
+    }
+}
